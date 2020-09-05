d2d70235138a2d282812b157ff1d9e479891354e
safeemmd <37902568+safeemmd@users.noreply.github.com>
2020-09-05 18:59:15 +0530

    Update TestGreeter.java

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -17,16 +17,16 @@ public class TestGreeter {
     greeter = new Greeter();
   }
 
-
+  @Test
   public void greetShouldIncludeTheOneBeingGreeted() {
-    String someone = "World"
+    String someone = "World";
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
 
   @Test
   public void greetShouldIncludeGreetingPhrase() {
-    String someone = "World"
+    String someone = "World";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
