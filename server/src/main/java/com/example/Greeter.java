414968e32cd3e31acfe76a8d3eb0fba6b0288b2a
safeemmd <37902568+safeemmd@users.noreply.github.com>
2020-09-05 18:49:31 +0530

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -5,17 +5,7 @@ package com.example;
  */
 public class Greeter {
 
-  /**
-   * This is a constructor.
-   */
-  public Greeter() {
 
-  }
-
-  /**
-   * @param someone the name of a person
-   * @return greeting string
-   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
